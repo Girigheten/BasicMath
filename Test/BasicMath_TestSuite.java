64b787b1f7a23fa666b4143873e36074d5d94bcd
Peter Englsperger <peter.englsperger@gmail.com>
2019-02-07 13:50:54 +0100

    added Tests Suite class.

---
+++ Test/BasicMath_TestSuite.java
@@ -0,0 +1,13 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+
+        //add testclasses here
+        BasicMathTest.class
+})
+
+public class BasicMath_TestSuite {
+    //Keep this empty
+}
